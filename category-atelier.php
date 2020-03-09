f40082b8d47829c94b38077a53ba41c8fd5021c9
Gregoire Maschio <gregmaschio@hotmail.com>
2020-03-09 09:54:54 -0400

    création du grid

--- category-atelier.php
+++ category-atelier.php
@@ -1,3 +1,4 @@
+
 <?php
 get_header();
 /////////////////////////////////ATELIER
@@ -13,13 +14,14 @@ $query1 = new WP_Query( $args );
 
 // echo '<div class ="conteneur-nouvelles">';
 // The 2nd Loop
+echo '<div class = "conteneur-atelier">';
 while ( $query1->have_posts() ) {
             $query1->the_post();
             echo '<div class ="posts-atelier">';
-            echo '<p> <a href ="' . get_permalink($id) . '">' . get_the_title() . " ________<span class='texteRouge'> " .get_post_field('post_name') . " </span><span class='texteBleu'>________ " .get_the_author_meta( 'display_name', $post->post_author )   . '</span></a></p>';
+            echo '<p><a href ="' . get_permalink($id) . '">' . get_the_title() . "</p><p>" .get_post_field('post_name') . "</p><p>" .get_the_author_meta( 'display_name', $post->post_author )   . '</a></p>';
             echo '</div>';
 }
-// echo '</div>';
+echo '</div>';
 // Restore original Post Data
 wp_reset_postdata();
 ?>
